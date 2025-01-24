8c54a4e1df5850897d262aa1616da375e9bce483
dhruvv1101 <dhruv83170@gmail.com>
2025-01-24 17:04:22 +0000

    conversion

---
+++ basics/03_conversionop.js
@@ -0,0 +1,32 @@
+let score1=null;
+let score2=3;
+let score3="33"
+let score4=undefined;
+let score5=Number(score3)
+let score6=Number(score1)
+let score7="";
+let score8=Boolean(score7);
+let score9=Boolean(score3);
+console.table([typeof(score1),typeof(score2),typeof(score3),typeof(score4),typeof(score5),typeof(score6),typeof(score7),typeof(score8),typeof(score9)]);
+console.table([score1,score2,score3,score4,score5,score6,score7,score8,score9]);
+/*0       │ null      │
+│ 1       │ 3         │
+│ 2       │ '33'      │
+│ 3       │ undefined │
+│ 4       │ 33        │
+│ 5       │ 0         │
+│ 6       │ ''        │
+│ 7       │ false     │
+│ 8       │ true   
+
+(index) │ Values      │
+├─────────┼─────────────┤
+│ 0       │ 'object'    │
+│ 1       │ 'number'    │
+│ 2       │ 'string'    │
+│ 3       │ 'undefined' │
+│ 4       │ 'number'    │
+│ 5       │ 'number'    │
+│ 6       │ 'string'    │
+│ 7       │ 'boolean'   │
+│ 8       │ 'boolean' */
