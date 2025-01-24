ebda8d1ca60a5c92996a77ed1506c146803d4947
dhruvv1101 <dhruv83170@gmail.com>
2025-01-24 16:50:09 +0000

    datatype

---
+++ basics/01_variable.js
@@ -0,0 +1,4 @@
+const gamer=6767;
+let name="Dhruv";
+console.log(gamer);
+console.table([gamer,name]);
---
+++ basics/02_datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+//alert(3+3);//
+//number=>2 ^53
+//bigint
+//boolean
+//null=>standalone value,type: object
+//string=>""
+//undefined=>type:undefined
+
